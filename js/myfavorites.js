8754cf5147c9a2a64f008ad89946bc375a80d326
prapti koirala <praptikoirala6@gmail.com>
2021-09-27 13:52:22 +0545

    my favorites display

--- js/myfavorites.js
+++ js/myfavorites.js
@@ -1,44 +1,91 @@
 import { getSignInUserID } from "../firebase/firestore.js";
 import { getRecipe, innerDetails } from "./imageInfo.js";
+import { hideLoader, showLoader } from "./loader.js";
 import { displayDetails, renderRecipes} from "./renderingrecipe.js";
 
 const firestore = firebase.firestore();
 
-document.querySelector(".favorites-container").addEventListener('click' , displayFavoriteRecipes);
+showLoader();
 
-function displayFavoriteRecipes(){
-   const favoritesSec = document.querySelector('.recipe-disp'); 
-   let result = '';
-   const userID = getSignInUserID();
-   const recipes = [];
+document.querySelector(".favorites-link").addEventListener('click' , displayFavoriteRecipes);
 
-   firestore.collection("ProjectUsers").doc(userID).collection("favorites").onSnapshot(snapshot => {
-      snapshot.docs.forEach(doc => {
-         recipes.push(doc.data());
-      });
-
-      result += renderRecipes(recipes);
-
-      favoritesSec.innerHTML = result;
-
-      favoritesSec.addEventListener('click' , (e) => {
-
-         if(e.target.classList.contains('recipeImage') || e.target.classList.contains('infoItem') ){
-            
-            document.querySelector('.favorites-container').style.display = 'none';
-            document.querySelector('.favorites-details-container').style.display = 'flex';
+function displayFavoriteRecipes(e){
    
-            const result = getRecipe(recipes, e);
+   e.preventDefault();
+
+   hideLoader();
    
-            const favoriteRecipeDetails = document.querySelector('.favorites-details-container');
-   
-            favoriteRecipeDetails.innerHTML = displayDetails(result);
+   e.target.classList.add('show-my-favorites');
+
+   if(e.target.classList.contains('show-my-favorites')){
+      const favoritesSec = document.querySelector('.recipe-disp'); 
+      let result = '';
+      const userID = getSignInUserID();
+      const recipes = [];
+
+      firestore.collection("ProjectUsers").doc(userID).collection("favorites").onSnapshot(snapshot =>{
+         snapshot.docs.forEach(doc => {
+            recipes.push(doc.data());
+         });
+
+         result += renderRecipes(recipes);
+
+         favoritesSec.innerHTML = result;
+
+         favoritesSec.addEventListener('click' , (e) => {
+
+            if(e.target.classList.contains('recipeImage') || e.target.classList.contains('infoItem') ){
+               
+               document.querySelector('.favorites-container').style.display = 'none';
+               document.querySelector('.favorites-details-container').style.display = 'flex';
       
-            innerDetails(result);
-         }
-   
+               const result = getRecipe(recipes, e);
+      
+               const favoriteRecipeDetails = document.querySelector('.favorites-details-container');
+      
+               favoriteRecipeDetails.innerHTML = displayDetails(result);
+         
+               innerDetails(result);
+            }
+      
+         })
       })
-   })
-
+   }
 }
+
+// function displayFavoriteRecipes(){
+   // const favoritesSec = document.querySelector('.recipe-disp'); 
+   // let result = '';
+   // const userID = getSignInUserID();
+   // const recipes = [];
+
+   // firestore.collection("ProjectUsers").doc(userID).collection("favorites").onSnapshot(snapshot => {
+   //    snapshot.docs.forEach(doc => {
+   //       recipes.push(doc.data());
+   //    });
+
+   //    result += renderRecipes(recipes);
+
+   //    favoritesSec.innerHTML = result;
+
+   //    favoritesSec.addEventListener('click' , (e) => {
+
+   //       if(e.target.classList.contains('recipeImage') || e.target.classList.contains('infoItem') ){
+            
+   //          document.querySelector('.favorites-container').style.display = 'none';
+   //          document.querySelector('.favorites-details-container').style.display = 'flex';
+   
+   //          const result = getRecipe(recipes, e);
+   
+   //          const favoriteRecipeDetails = document.querySelector('.favorites-details-container');
+   
+   //          favoriteRecipeDetails.innerHTML = displayDetails(result);
+      
+   //          innerDetails(result);
+   //       }
+   
+   //    })
+   // })
+
+// }
   
